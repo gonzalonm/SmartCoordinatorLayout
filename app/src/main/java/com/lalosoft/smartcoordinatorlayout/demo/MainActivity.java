ef775867aa7f747a4806424647766d1d603d65c9
lalosoft <lalosoft@gmail.com>
2016-10-06 16:03:37 -0300

    RecyclerView working with provided adapter

--- app/src/main/java/com/lalosoft/smartcoordinatorlayout/demo/MainActivity.java
+++ app/src/main/java/com/lalosoft/smartcoordinatorlayout/demo/MainActivity.java
@@ -2,8 +2,16 @@ package com.lalosoft.smartcoordinatorlayout.demo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.lalosoft.smartcoordinatorlayout.SmartCoordinatorLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +20,62 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewGroup rootView = (ViewGroup) findViewById(R.id.activity_main);
+
         // build SmartCoordinatorLayout
         SmartCoordinatorLayout
                 smartCoordinatorLayout = new SmartCoordinatorLayout.Builder(this)
+                .buildWithView(rootView)
                 .useFloatingActionButton(SmartCoordinatorLayout.FABType.NONE)
+                .setRecyclerViewAdapter(new MyAdapter(createStringList()))
                 .build();
 
         smartCoordinatorLayout.setup();
+    }
+
+    private List<String> createStringList() {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add("Item #" + i);
+        }
+        return list;
+    }
+
+    /**
+     * Implements the custom adapter to use in the RecyclerView
+     */
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+        private List<String> list;
+
+        public MyAdapter(List<String> list) {
+            this.list = list;
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.view_item, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.text.setText(list.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+
+            TextView text;
+
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                text = (TextView) itemView.findViewById(R.id.item_text);
+            }
+        }
     }
 }
--- library/src/main/java/com/lalosoft/smartcoordinatorlayout/SmartCoordinatorLayout.java
+++ library/src/main/java/com/lalosoft/smartcoordinatorlayout/SmartCoordinatorLayout.java
@@ -3,7 +3,13 @@ package com.lalosoft.smartcoordinatorlayout;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * Created by Gonzalo.Martin on 9/30/2016
@@ -16,8 +22,18 @@ public class SmartCoordinatorLayout {
     // Vars from Builder
     private Context mContext;
     private FloatingActionButtonListener mFloatingActionButtonListener;
-    private View mView;
+    private ViewGroup rootView;
     private FABType mFabType;
+    private RecyclerView.Adapter mRecyclerViewAdapter;
+    private List<?> mRecyclerViewData;
+    private RecyclerView.ViewHolder mRecyclerViewHolder;
+    private SmartRecyclerAdapterCallback mRecyclerAdapterCallback;
+
+    private RecyclerView mRecyclerView;
+
+    public interface SmartRecyclerAdapterCallback {
+        void onBindSmartViewHolder(RecyclerView.ViewHolder viewHolder, Object data);
+    }
 
     public interface FloatingActionButtonListener {
 
@@ -33,15 +49,19 @@ public class SmartCoordinatorLayout {
     public static class Builder {
         private Context context;
         private FloatingActionButtonListener floatingActionButtonListener;
-        private View view;
+        private ViewGroup viewGroup = null;
         private FABType fabType;
+        private RecyclerView.Adapter recyclerViewAdapter;
+        private List<?> list;
+        private RecyclerView.ViewHolder recyclerViewHolder;
+        private SmartRecyclerAdapterCallback recyclerAdapterCallback;
 
         public Builder(@NonNull Context context) {
             this.context = context;
         }
 
-        public Builder buildWithView(@NonNull View view) {
-            this.view = view;
+        public Builder buildWithView(@NonNull ViewGroup view) {
+            this.viewGroup = view;
             return this;
         }
 
@@ -61,17 +81,57 @@ public class SmartCoordinatorLayout {
             return this;
         }
 
+        public Builder setRecyclerViewAdapter(@NonNull RecyclerView.Adapter recyclerViewAdapter) {
+            this.recyclerViewAdapter = recyclerViewAdapter;
+            return this;
+        }
+
+        public Builder setRecyclerViewData(@NonNull List<?> list, @NonNull RecyclerView.ViewHolder viewHolder,
+                                           SmartRecyclerAdapterCallback recyclerAdapterCallback) {
+            this.list = list;
+            this.recyclerViewHolder = viewHolder;
+            this.recyclerAdapterCallback = recyclerAdapterCallback;
+            return this;
+        }
+
         public SmartCoordinatorLayout build() {
+            if (viewGroup == null) {
+                throw new IllegalStateException("View cannot be null");
+            }
             SmartCoordinatorLayout smartCoordinatorLayout = new SmartCoordinatorLayout();
             smartCoordinatorLayout.mContext = this.context;
             smartCoordinatorLayout.mFabType = fabType;
             smartCoordinatorLayout.mFloatingActionButtonListener = floatingActionButtonListener;
-            smartCoordinatorLayout.mView = view;
+            smartCoordinatorLayout.rootView = viewGroup;
+            smartCoordinatorLayout.mRecyclerViewAdapter = recyclerViewAdapter;
+            smartCoordinatorLayout.mRecyclerViewData = list;
+            smartCoordinatorLayout.mRecyclerViewHolder = recyclerViewHolder;
+            smartCoordinatorLayout.mRecyclerAdapterCallback = recyclerAdapterCallback;
             return smartCoordinatorLayout;
         }
     }
 
     public void setup() {
-        // TODO: implement the setup of this control
+        View view = LayoutInflater.from(mContext).inflate(R.layout.layout_main, rootView);
+        mCoordinatorLayout = (CoordinatorLayout) view.findViewById(R.id.coordinator_layout);
+
+        // setup RecyclerView component
+        setupRecyclerView(mCoordinatorLayout);
+    }
+
+    private void setupRecyclerView(View view) {
+        // find view by id
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+
+        if (mRecyclerViewAdapter == null) {
+            // use custom adapter
+            mRecyclerView.setAdapter(new CustomRecyclerViewAdapter(mRecyclerAdapterCallback,
+                    mRecyclerViewData, mRecyclerViewHolder));
+        } else {
+            // use provided adapter
+            mRecyclerView.setAdapter(mRecyclerViewAdapter);
+        }
     }
 }
